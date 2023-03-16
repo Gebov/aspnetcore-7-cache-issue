c233b706a33c85f62b6bf36ba1fac9551dad8258
gebov <martin.gebov@gmail.com>
2023-03-16 16:28:47 +0200

    wip

--- HomeController.cs
+++ HomeController.cs
@@ -6,12 +6,6 @@ public class HomeController : Controller
     [OutputCache(PolicyName = "default")]
     public IActionResult Index()
     {
-        var ocFeature = this.HttpContext.Features.Get<IOutputCacheFeature>();
-        if (ocFeature != null)
-        {
-            ocFeature.Context.CacheVaryByRules.QueryKeys = new Microsoft.Extensions.Primitives.StringValues("page");
-        }
-
         return this.View();
     }
 }
